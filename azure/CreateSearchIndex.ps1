0f20b53a050c11fa95f60c2797a2749fb0ca2859
Simon Bull <simon.bull.84@googlemail.com>
2019-12-18 07:34:00 +0000

    Change create search index script to take admin key

--- azure/CreateSearchIndex.ps1
+++ azure/CreateSearchIndex.ps1
@@ -1,15 +1,17 @@
 param (
-    [string] $ResourceGroup,
+    #[string] $ResourceGroup,
     [string] $ServiceName,
+    [string] $AdminKey,
     [string] $IndexName,
-    [string] $schemaPath
+    [string] $SchemaPath
 )
 
-$acsKeys = Get-AzSearchAdminKeyPair -ResourceGroup $ResourceGroup -ServiceName $ServiceName
+# does not currently seem to be supported in devops
+# $acsKeys = Get-AzSearchAdminKeyPair -ResourceGroup $ResourceGroup -ServiceName $ServiceName
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", 'application/json')
-$headers.Add("api-key", $acsKeys.Primary)
+$headers.Add("api-key", $AdminKey)
 
 $response = Invoke-RestMethod -TimeoutSec 10000 "https://$($ServiceName).search.windows.net/indexes/$($IndexName)?api-version=2019-05-06" -Method Put -Headers $headers -Body "$(get-content $schemaPath)"
 Write-Host $response
